509053a775f3f8016f163769f9c0d74d9cda0478
lawince <106462896+Netoerare@users.noreply.github.com>
2024-11-15 21:28:26 +0800

    Updatedd

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -53,7 +53,7 @@ using System.Threading.Tasks;
         {
             int damageReduction = Armor / 2;
             Random rnd = new Random();
-            bool block = rnd.Next(100) < 15; // 15% chance to block
+            bool block = rnd.Next(100) < 15; 
 
             if (block)
             {
@@ -74,4 +74,74 @@ using System.Threading.Tasks;
 
             Console.WriteLine($"{Name} leveled up! Level: {Level}, Strength: {Strength}, Health: {Health}, Armor: {Armor}");
         }
-}
+    
+
+    class Mage : GameCharacter
+    {
+        public int SpellPower { get; set; } = 10;
+
+        public Mage(string name, int level, int health, int mana, int intelligence)
+            : base(name, level, health, mana, 0, intelligence)
+        {
+        }
+
+        public override void Attack()
+        {
+            int magicDamage = (Intelligence * 3) + SpellPower;
+            Random random = new Random();
+            bool burningEffect = random.Next(100) < 25; 
+
+            Console.WriteLine($"{Name} casts a spell and deals {magicDamage} damage{(burningEffect ? ", applying a burning effect!" : ".")}");
+        }
+
+        public override void Defend()
+        {
+            int damageReduction = Mana / 4;
+            Random random = new Random();
+            bool evade = random.Next(100) < 20; 
+
+            if (evade)
+            {
+                Console.WriteLine($"{Name} evades the attack completely!");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} reduces the damage by {damageReduction}.");
+            }
+        }
+
+        public override void LevelUp()
+        {
+            Level++;
+            Intelligence += 5;
+            Mana += 15;
+            SpellPower += 3;
+
+            Console.WriteLine($"{Name} leveled up! Level: {Level}, Intelligence: {Intelligence}, Mana: {Mana}, SpellPower: {SpellPower}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior thor = new Warrior("Thor", 1, 100, 15);
+            Mage merlin = new Mage("Merlin", 1, 80, 50, 20);
+
+            Console.WriteLine($"Warrior: {thor.Name}, Level: {thor.Level}, Health: {thor.Health}, Strength: {thor.Strength}, Armor: {thor.Armor}");
+            Console.WriteLine($"Mage: {merlin.Name}, Level: {merlin.Level}, Health: {merlin.Health}, Mana: {merlin.Mana}, Intelligence: {merlin.Intelligence}, SpellPower: {merlin.SpellPower}");
+
+            Console.WriteLine("\n--- Actions ---");
+            thor.Attack();
+            thor.Defend();
+            thor.LevelUp();
+
+            merlin.Attack();
+            merlin.Defend();
+            merlin.LevelUp();
+
+            Console.WriteLine("\n--- Updated State ---");
+            Console.WriteLine($"Warrior: {thor.Name}, Level: {thor.Level}, Health: {thor.Health}, Strength: {thor.Strength}, Armor: {thor.Armor}");
+            Console.WriteLine($"Mage: {merlin.Name}, Level: {merlin.Level}, Health: {merlin.Health}, Mana: {merlin.Mana}, Intelligence: {merlin.Intelligence}, SpellPower: {merlin.SpellPower}");
+        }
+    }
